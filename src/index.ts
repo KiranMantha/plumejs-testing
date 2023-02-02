9b819516a3d53249f183ff72ed0ef4293c78934c
KiranMantha <kiranv.mantha@gmail.com>
2023-02-02 18:57:20 +0530

    WIP: started migrating testbed

---
+++ src/index.ts
@@ -0,0 +1,48 @@
+import { Injector, InjectionToken } from '@plumejs/core';
+
+type ConstructorType<T extends { new (...args: any[]): T }> = T;
+
+export interface Fixture<T> {
+  componentInstance: T;
+  element: ShadowRoot;
+}
+
+export class TestBed {
+  static async MockComponent<T>(
+    target: ThisType<T>,
+    mockDependencies?: { provider: ConstructorType<any>; useValue: any }[]
+  ): Promise<Fixture<T>> {
+    if (mockDependencies) {
+      for (const { provider, useValue } of mockDependencies) {
+        TestBed.MockService(provider, useValue);
+      }
+    }
+    const appRoot: any = await _waitForComponentToRender((target as any).prototype.selector);
+    return { componentInstance: appRoot.getInstance(), element: appRoot.shadowRoot };
+  }
+
+  static MockService(klass: ConstructorType<any>, mockedInstance: any) {
+    //Injector.removeService(klass);
+    return InjectionToken(klass, mockedInstance);
+  }
+
+  static RemoveComponent<T>(fixture: Fixture<T>) {
+    document.body.removeChild(fixture.element.host);
+  }
+}
+
+async function _waitForComponentToRender(tag: string) {
+  const ele = document.createElement(tag);
+  document.body.appendChild(ele);
+  return new Promise((resolve) => {
+    function requestComponent() {
+      const element = document.querySelector(tag);
+      if (element) {
+        resolve(element);
+      } else {
+        window.requestAnimationFrame(requestComponent);
+      }
+    }
+    requestComponent();
+  });
+}
